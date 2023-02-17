26448e74bb4b4f399c38abea0dcf95654d9baa1c
Rainer Leber <rainer.leber@sva.de>
2023-02-17 13:24:20 +0000

    bump version

--- gardener/config.go
+++ gardener/config.go
@@ -9,7 +9,7 @@ import (
 	"os"
 	"strings"
 
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
